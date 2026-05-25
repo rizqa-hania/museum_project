c7c39ee24e6193072c8bc75878949fe5964e073d
rizqa-hania <rizqa.hania39@smk.belajar.id>
2026-05-25 11:06:13 +0700

    rizqa

--- app/Tiket.php
+++ app/Tiket.php
@@ -9,4 +9,9 @@ class Tiket extends Model
     protected $table = 'tiket';
     protected $primaryKey = 'tiket_id';
     protected $fillable = ['kategori_id', 'pengunjung_id', 'petugas_id', 'tanggal_kunjungan'];
+    
+    public function kategori()
+    {
+        return $this->belongsTo(KategoriTiket::class, 'kategori_id');
+    }
 }
--- app/pengunjung.php
+++ app/pengunjung.php
@@ -9,4 +9,9 @@ class Pengunjung extends Model
     protected $table = 'pengunjung';
     protected $primaryKey = 'pengunjung_id';
     protected $fillable = ['nama', 'email', 'no_hp'];
+    
+    public function transaksi()
+    {
+        return $this->hasMany(Transaksi::class, 'pengunjung_id');
+    }
 }
